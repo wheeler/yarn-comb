0c2dc25eacbefbcab0e9a920c85a0d06f0f38a94
Mark Wheeler <mark.wheeler@gusto.com>
2022-06-22 15:16:29 -0700

    Add GitHub actions CI testing (#1)
    is this easy? yeah not bad.

--- test/run-tests.sh
+++ test/run-tests.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+CYAN='\033[1;36m'
+WHITE='\033[1;37m' # overrides github grey default
+NC='\033[0m' # No Color
+
 failed=0
 
 for d in test-projects/*/ ; do
-  echo ==== TEST ${d%/}
-  cd $d
+  echo -e "\n${CYAN}TEST${WHITE}   ${d%/}${NC}\n"
+  cd $d || exit
   npx yarn-comb --fix
-  cd ../..
+  cd ../.. || exit
   ./test-lockfile-snapshot.sh $d || failed=1
 done
 
--- test/test-lockfile-snapshot.sh
+++ test/test-lockfile-snapshot.sh
@@ -5,14 +5,20 @@ testpath=${1%/} # remove trailing slash if present
 cmp --quiet $testpath/yarn.lock $testpath/yarn.lock.snap
 cmpcode=$?
 
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+WHITE='\033[1;37m' # overrides github grey default
+NC='\033[0m' # No Color
 
 if [ $cmpcode == 0 ]
 then
-  echo "PASS  \`$testpath/yarn.lock\` file matched \`$testpath/yarn.lock.snap\`."
+  echo -e "\`$testpath/yarn.lock\` file matched \`$testpath/yarn.lock.snap\`."
+  echo -e "\n${GREEN}PASS${WHITE}   $testpath${NC}\n"
   exit 0
 elif [ $cmpcode == 1 ] # cmp command will exit with code 1 if there is any difference
 then
-  echo "FAIL  \`$testpath/yarn.lock\` file had differences from \`$testpath/yarn.lock.snap\`."
+  echo -e "\`$testpath/yarn.lock\` file had differences from \`$testpath/yarn.lock.snap\`."
+  echo -e "\n${RED}FAIL${White}   $testpath${NC}\n"
   # comm command will output details
   comm -3 $testpath/yarn.lock $testpath/yarn.lock.snap
   exit 1
